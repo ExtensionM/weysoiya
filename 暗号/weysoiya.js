d4a15d706c7d29b079b5d2e62ec92c7a7168d757
Rafmagn <skelfilegur_herbergi-sonota@yahoo.co.jp>
2014-11-12 14:57:09 +0900

    出力をクリップボードにコピーできるボタンを追加

--- 暗号/weysoiya.js
+++ 暗号/weysoiya.js
@@ -1,7 +1,13 @@
-﻿
-function outp_into_inp() {
+﻿function outp_into_inp() {
     var str = document.getElementById("output").innerHTML;
+    str = str.replace(/<br>/g, "\n");
     document.getElementById("input").value = str;
+}
+function outp_copy() {
+    var str = document.getElementById("output").innerHTML;
+    str = str.replace(/<br>/g, "\n");
+    clipboardData.setData('text', str);
+
 }
 
 function code() {
@@ -13,30 +19,28 @@ function code() {
         int[i] = a.slice(i, i + 1);
         if (int[i] == "\n") {
             //改行の処理
-            int[i] = "うぇいうぇいうぇいうぇいうぇいうぇいうぇいうぇいうぇいうぇい";
+            int[i] = "うぇいうぇいうぇいうぇいうぇいうぇいうぇいうぇい";
             document.getElementById("output").innerHTML += int[i];
         }
         else {
             int[i] = escape(int[i]);
             if (int[i].indexOf("%u") != -1) {
+                //普通のユニコード文字
                 int[i] = ("00000000" + (parseInt(int[i].replace("%u", ""), 16).toString(4))).slice(-8);
-                int[i] = "45" + int[i];
             }
             else if (int[i].indexOf("%") != -1) {
+                //半角カッコとか
                 int[i] = ("00000000" + (parseInt(int[i].replace("%", ""), 16).toString(4))).slice(-8);
-                int[i] = "40" + int[i];
             }
             else {
+                //半角英数字
                 int[i] = ("00000000" + (int[i].charCodeAt(0).toString(4))).slice(-8);
-                int[i] = "00" + int[i];
             }
             int[i] = int[i]
                 .replace(/0/g, "うぇい")
                 .replace(/1/g, "そいや")
                 .replace(/2/g, "ウェイ")
-                .replace(/3/g, "ソイヤ")
-                .replace(/4/g, "ｳｪｲ")
-                .replace(/5/g, "ｿｲﾔ");
+                .replace(/3/g, "ソイヤ");
             document.getElementById("output").innerHTML += int[i];
         }
         i++;
@@ -51,34 +55,23 @@ function decode() {
     var a = document.getElementById("input").value;
     var int = new Array();
     var i = 0, timer = setInterval(function () {
-        int[i] = a.slice(30 * i, 30 * i + 30);
+        int[i] = a.slice(24 * i, 24 * i + 24);
         int[i] = int[i].replace(/うぇい/g, "0")
         .replace(/そいや/g, "1")
         .replace(/ウェイ/g, "2")
-        .replace(/ソイヤ/g, "3")
-        .replace(/ｳｪｲ/g, "4")
-        .replace(/ｿｲﾔ/g, "5");
-        if (int[i] == "0000000000") {
+        .replace(/ソイヤ/g, "3");
+        if (int[i] == "00000000") {
             //改行
             int[i] = "<br>";
             document.getElementById("output").innerHTML += int[i];
         }
         else {
-            if (int[i].substr(0, 2) == "45") {
-                int[i] = parseInt(int[i].substr(2, 8), 4).toString(16);
-                int[i] = unescape("%u" + ("0000"+int[i]).slice(-4));
-            }
-            else if (int[i].substr(0, 2) == "40") {
-                int[i] = parseInt(int[i].substr(2, 8), 4).toString(16);
-                int[i] = unescape("%" + ("00" + int[i]).slice(-2));
-            }
-            else if (int[i].substr(0, 2) == "00") {
-                int[i] = String.fromCharCode(parseInt(int[i].substr(2, 8), 4));
-            }
+            int[i] = parseInt(int[i], 4).toString(16);
+            int[i] = unescape("%u" + ("0000" + int[i]).slice(-4));
             document.getElementById("output").innerHTML += int[i];
         }
         i++;
-        if (i >= a.length / 30) {
+        if (i >= a.length / 24) {
             clearInterval(timer);
         }
     }, 0);
